b295d7a57be0ae42b71aa7e5d77fd48ebd24a99a
James Makela <jamesilmarimakela@outlook.com>
2026-03-26 09:51:18 +0800

    Remove unnecessary intel items from AMD deps and clean up tabs

--- nixos/modules/amd-deps.nix
+++ nixos/modules/amd-deps.nix
@@ -1,23 +1,16 @@
 { pkgs, config, ... }:
 
 {
-	environment.systemPackages = with pkgs; [
-		egl-wayland	
-		mesa
+  environment.systemPackages = with pkgs; [
+    egl-wayland
+    mesa
     lm_sensors
-	];
+  ];
 
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [
-      intel-compute-runtime
-      intel-media-driver
-      intel-ocl
-      intel-vaapi-driver
-      libvdpau-va-gl
-    ];
   };
 
   hardware.amdgpu.initrd.enable = true; # sets boot.initrd.kernelModules = ["amdgpu"];
